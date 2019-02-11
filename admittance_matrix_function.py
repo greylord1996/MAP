361bda58f71ab6e877b3763788ce2c8df947f4ee
greylord1996 <emchinov@bk.ru>
2019-02-12 02:23:44 +0300

    preparing for usage as module

--- admittance_matrix_function.py
+++ admittance_matrix_function.py
@@ -13,14 +13,12 @@ def create_admittance_matrix_function():
 
     # System DAE Model
     Pe = Vm_a*Ef_a*sin(del_a-Va_a)/X_Ya
-
     F_vec = Matrix([w_a, (Pm_a - Pe - D_Ya*w_a)/M_Ya])
 
     # Output => [abs(I); angle(I)], where I flows into the generator
     G_vec = Matrix([(1/X_Ya)*sqrt(Vm_a ** 2 + Ef_a ** 2 - 2*Ef_a*Vm_a*cos(Va_a-del_a)),
                    atan((Vm_a*sin(Va_a)-Ef_a*sin(del_a))/(Vm_a*cos(Va_a)-Ef_a*cos(del_a)))-pi])
 
-    Omega_a = symbols('Omega_a')
 
     JacFX = F_vec.jacobian(X)
     JacFUv = F_vec.jacobian(Uv)
@@ -28,8 +26,11 @@ def create_admittance_matrix_function():
     JacGUv = G_vec.jacobian(Uv)
 
 
+    Omega_a = symbols('Omega_a')
     Ys = (JacGX/(j*Omega_a*eye(len(X)) - JacFX))*JacFUv + JacGUv
-    #Ys = simplify(Ys)
-
     Ys_compute = lambdify((Ef_a, D_Ya, M_Ya, X_Ya, Omega_a, Va_a, Vm_a, del_a), Ys, 'numpy')
+
     return Ys_compute
+
+
+Ys_compute = create_admittance_matrix_function()
