2bc175a732cb92c7239bbfa215bfbc2af51572cd
codepictor <grozzmaster@gmail.com>
2019-03-02 19:08:33 +0300

    Store admittance matrix on the disk

--- src/create_admittance_matrix.py
+++ src/create_admittance_matrix.py
@@ -1,47 +1,87 @@
+import os
+import os.path
+import pickle
+
 import sympy as sp
 from sympy import *
 import numpy as np
 
-j = np.complex(0, 1)
+import utils
 
-del_a, w_a, Pm_a, Omega, Vm_a, Va_a = symbols('del_a w_a Pm_a Omega Vm_a Va_a')
+
+
 Ef_a = symbols('Ef_a', real=True)
 D_Ya = symbols('D_Ya', real=True)
 X_Ya = symbols('X_Ya', real=True)
 M_Ya = symbols('M_Ya', real=True)
 
-
-# X = symbols('X')
-X = Array([del_a, w_a])
-Uv = Array([Vm_a, Va_a])
-X_params = Array([D_Ya, Ef_a, M_Ya, X_Ya])
-
-
-# System DAE Model
-Pe = Vm_a*Ef_a*sin(del_a-Va_a)/X_Ya
-
-F_vec = Matrix([w_a, (Pm_a - Pe - D_Ya*w_a)/M_Ya])
-
-# Output => [abs(I); angle(I)], where I flows into the generator
-G_vec = Matrix([(1/X_Ya)*sqrt(Vm_a ** 2 + Ef_a ** 2 - 2*Ef_a*Vm_a*cos(Va_a-del_a)),
-               atan((Vm_a*sin(Va_a)-Ef_a*sin(del_a))/(Vm_a*cos(Va_a)-Ef_a*cos(del_a)))-pi])
-
 Omega_a = symbols('Omega_a', real=True)
-JacFX = F_vec.jacobian(X)
-JacFUv = F_vec.jacobian(Uv)
-JacGX = G_vec.jacobian(X)
-JacGUv = G_vec.jacobian(Uv)
 
 
-Ys = (JacGX/(j*Omega_a*eye(len(X)) - JacFX))*JacFUv + JacGUv
-Ys = Ys.subs(Va_a, 0.5)
-Ys = Ys.subs(del_a, 1)
-Ys = Ys.subs(Vm_a, 1)
-#Ys = simplify(Ys)
 
-Ys_compute = lambdify((Ef_a, D_Ya, M_Ya, X_Ya, Omega_a), Ys, 'numpy')
+@utils.singleton
+class AdmittanceMatrix:
+    """A singletone representing the admittance matrix.
 
-#Ys_compute = lambdify((w_a, Ef_a, D_Ya, M_Ya, X_Ya, Pm_a, Omega, Omega_a, Va_a, Vm_a, del_a), Ys, 'numpy')
+    Normally, it is not necessary to recompute this matrix
+    every time when you create an instance of this class.
+    But if you want to force recomputation from the scratch
+    you should provide one argument for constructor:
+    >>> Ys = AdmittanceMatrix(is_actual=False).Ys
+
+    Attributes:
+        Ys: admittance matrix
+    """
+
+    def __init__(self, is_actual=True):
+        path_to_matrix_file = os.path.join(
+            '..', 'data', 'precomputed', 'admittance_matrix.pickle'
+        )
+        if not is_actual:
+            j = np.complex(0, 1)
+            del_a, w_a, Pm_a, Omega, Vm_a, Va_a = symbols(
+                'del_a w_a Pm_a Omega Vm_a Va_a'
+            )
+
+            X = Array([del_a, w_a])
+            Uv = Array([Vm_a, Va_a])
+
+            # System DAE Model
+            Pe = Vm_a*Ef_a*sin(del_a-Va_a)/X_Ya
+
+            F_vec = Matrix([w_a, (Pm_a - Pe - D_Ya*w_a)/M_Ya])
+
+            # Output => [abs(I); angle(I)], where I flows into the generator
+            G_vec = Matrix([
+                (1/X_Ya)*sqrt(Vm_a**2 + Ef_a**2 - 2*Ef_a*Vm_a*cos(Va_a-del_a)),
+                atan((Vm_a*sin(Va_a)-Ef_a*sin(del_a))/(Vm_a*cos(Va_a)-Ef_a*cos(del_a)))-pi
+            ])
+
+            JacFX = F_vec.jacobian(X)
+            JacFUv = F_vec.jacobian(Uv)
+            JacGX = G_vec.jacobian(X)
+            JacGUv = G_vec.jacobian(Uv)
+
+            Ys = (JacGX/(j*Omega_a*eye(len(X)) - JacFX))*JacFUv + JacGUv
+            Ys = Ys.subs(Va_a, 0.5)
+            Ys = Ys.subs(del_a, 1)
+            self.Ys = Ys.subs(Vm_a, 1)
+
+            # Ys = simplify(Ys)
+            # self.Ys_compute = lambdify((Ef_a, D_Ya, M_Ya, X_Ya, Omega_a), Ys, 'numpy')
+            # Ys_compute = lambdify(
+            #     (w_a, Ef_a, D_Ya, M_Ya, X_Ya, Pm_a, Omega, Omega_a, Va_a, Vm_a, del_a),
+            #     Ys, 'numpy'
+            # )
+            pickle.dump(self.Ys, open(path_to_matrix_file, 'wb'))
+
+        else:
+            self.Ys = pickle.load(open(path_to_matrix_file, 'rb'))
+
+
+
+X_params = Array([D_Ya, Ef_a, M_Ya, X_Ya])
+Ys = AdmittanceMatrix().Ys
 
 Y11 = Ys[0, 0]
 Y12 = Ys[0, 1]
