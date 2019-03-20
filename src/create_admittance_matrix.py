d1222731cb31646861b20367cff6cfd0dc6c1a8b
greylord1996 <emchinov@bk.ru>
2019-03-21 01:25:55 +0300

    start work with blocks in covariance matrix

--- src/create_admittance_matrix.py
+++ src/create_admittance_matrix.py
@@ -83,50 +83,52 @@ class AdmittanceMatrix:
 
 
 
-Ys = AdmittanceMatrix().Ys
-Ys_compute = lambdify((Ef_a, D_Ya, M_Ya, X_Ya, Omega_a), Ys, 'numpy')
-
-Y11 = Ys[0, 0]
-Y12 = Ys[0, 1]
-Y21 = Ys[1, 0]
-Y22 = Ys[1, 1]
-
-Y11r = re(Y11)
-Y11i = im(Y11)
-Y12r = re(Y12)
-Y12i = im(Y12)
-Y21r = re(Y21)
-Y21i = im(Y21)
-Y22r = re(Y22)
-Y22i = im(Y22)
-
-Vmrs, Vmis, Vars, Vais, Imrs, Imis, Iars, Iais = symbols('Vmrs Vmis Vars Vais Imrs Imis Iars Iais')
-Inj_mrs, Inj_mis, Inj_prs, Inj_pis = symbols('Inj_mrs Inj_mis Inj_prs Inj_pis')
-
-
-Mr = Imrs - Y11r * Vmrs + Y11i * Vmis - Y12r * Vars + Y12i * Vais
-Mi = Imis - Y11i * Vmrs - Y11r * Vmis - Y12i * Vars - Y12r * Vais
-Pr = Iars - Y21r * Vmrs + Y21i * Vmis - Y22r * Vars + Y22i * Vais
-Pi = Iais - Y21i * Vmrs - Y21r * Vmis - Y22i * Vars - Y22r * Vais
-
-
-Mrtemp = Matrix([Imrs - Y11r * Vmrs + Y11i * Vmis - Y12r * Vars + Y12i * Vais])
-Mitemp = Matrix([Imis - Y11i * Vmrs - Y11r * Vmis - Y12i * Vars - Y12r * Vais])
-Prtemp = Matrix([Iars - Y21r * Vmrs + Y21i * Vmis - Y22r * Vars + Y22i * Vais])
-Pitemp = Matrix([Iais - Y21i * Vmrs - Y21r * Vmis - Y22i * Vars - Y22r * Vais])
-
-Grad_Mr = Mrtemp.jacobian(X_params)
-Grad_Mi = Mitemp.jacobian(X_params)
-Grad_Pr = Prtemp.jacobian(X_params)
-Grad_Pi = Pitemp.jacobian(X_params)
-
-
-Mr_compute = lambdify((D_Ya, Ef_a, M_Ya, X_Ya, Omega_a, Imrs, Vmrs, Vmis, Vars, Vais), Mr, 'numpy')
-Mi_compute = lambdify((Imis, Vmrs, Vmis, Vars, Vais, D_Ya, Ef_a, M_Ya, X_Ya, Omega_a), Mi, 'numpy')
-Pr_compute = lambdify((Iars, Vmrs, Vmis, Vars, Vais, D_Ya, Ef_a, M_Ya, X_Ya, Omega_a), Pr, 'numpy')
-Pi_compute = lambdify((Iais, Vmrs, Vmis, Vars, Vais, D_Ya, Ef_a, M_Ya, X_Ya, Omega_a), Pi, 'numpy')
-
-Grad_Mr_compute = lambdify((Vmrs, Vmis, Vars, Vais, D_Ya, Ef_a, M_Ya, X_Ya, Omega_a), Grad_Mr, 'numpy')
-Grad_Mi_compute = lambdify((Vmrs, Vmis, Vars, Vais, D_Ya, Ef_a, M_Ya, X_Ya, Omega_a), Grad_Mi, 'numpy')
-Grad_Pr_compute = lambdify((Vmrs, Vmis, Vars, Vais, D_Ya, Ef_a, M_Ya, X_Ya, Omega_a), Grad_Pr, 'numpy')
-Grad_Pi_compute = lambdify((Vmrs, Vmis, Vars, Vais, D_Ya, Ef_a, M_Ya, X_Ya, Omega_a), Grad_Pi, 'numpy')
+# Ys = AdmittanceMatrix().Ys
+# Ys_compute = lambdify((Ef_a, D_Ya, M_Ya, X_Ya, Omega_a), Ys, 'numpy')
+#
+# Y11 = Ys[0, 0]
+# Y12 = Ys[0, 1]
+# Y21 = Ys[1, 0]
+# Y22 = Ys[1, 1]
+#
+# Y11r = re(Y11)
+# Y11i = im(Y11)
+# Y12r = re(Y12)
+# Y12i = im(Y12)
+# Y21r = re(Y21)
+# Y21i = im(Y21)
+# Y22r = re(Y22)
+# Y22i = im(Y22)
+#
+# Vmrs, Vmis, Vars, Vais, Imrs, Imis, Iars, Iais = symbols('Vmrs Vmis Vars Vais Imrs Imis Iars Iais')
+# Inj_mrs, Inj_mis, Inj_prs, Inj_pis = symbols('Inj_mrs Inj_mis Inj_prs Inj_pis')
+#
+#
+# Mr = Imrs - Y11r * Vmrs + Y11i * Vmis - Y12r * Vars + Y12i * Vais
+# Mi = Imis - Y11i * Vmrs - Y11r * Vmis - Y12i * Vars - Y12r * Vais
+# Pr = Iars - Y21r * Vmrs + Y21i * Vmis - Y22r * Vars + Y22i * Vais
+# Pi = Iais - Y21i * Vmrs - Y21r * Vmis - Y22i * Vars - Y22r * Vais
+#
+#
+# Mrtemp = Matrix([Imrs - Y11r * Vmrs + Y11i * Vmis - Y12r * Vars + Y12i * Vais])
+# Mitemp = Matrix([Imis - Y11i * Vmrs - Y11r * Vmis - Y12i * Vars - Y12r * Vais])
+# Prtemp = Matrix([Iars - Y21r * Vmrs + Y21i * Vmis - Y22r * Vars + Y22i * Vais])
+# Pitemp = Matrix([Iais - Y21i * Vmrs - Y21r * Vmis - Y22i * Vars - Y22r * Vais])
+#
+# Grad_Mr = Mrtemp.jacobian(X_params)
+# Grad_Mi = Mitemp.jacobian(X_params)
+# Grad_Pr = Prtemp.jacobian(X_params)
+# Grad_Pi = Pitemp.jacobian(X_params)
+#
+#
+# Mr_compute = lambdify((D_Ya, Ef_a, M_Ya, X_Ya, Omega_a, Imrs, Vmrs, Vmis, Vars, Vais), Mr, 'numpy')
+# Mi_compute = lambdify((Imis, Vmrs, Vmis, Vars, Vais, D_Ya, Ef_a, M_Ya, X_Ya, Omega_a), Mi, 'numpy')
+# Pr_compute = lambdify((Iars, Vmrs, Vmis, Vars, Vais, D_Ya, Ef_a, M_Ya, X_Ya, Omega_a), Pr, 'numpy')
+# Pi_compute = lambdify((Iais, Vmrs, Vmis, Vars, Vais, D_Ya, Ef_a, M_Ya, X_Ya, Omega_a), Pi, 'numpy')
+#
+# Grad_Mr_compute = lambdify((Vmrs, Vmis, Vars, Vais, D_Ya, Ef_a, M_Ya, X_Ya, Omega_a), Grad_Mr, 'numpy')
+# Grad_Mi_compute = lambdify((Vmrs, Vmis, Vars, Vais, D_Ya, Ef_a, M_Ya, X_Ya, Omega_a), Grad_Mi, 'numpy')
+# Grad_Pr_compute = lambdify((Vmrs, Vmis, Vars, Vais, D_Ya, Ef_a, M_Ya, X_Ya, Omega_a), Grad_Pr, 'numpy')
+# Grad_Pi_compute = lambdify((Vmrs, Vmis, Vars, Vais, D_Ya, Ef_a, M_Ya, X_Ya, Omega_a), Grad_Pi, 'numpy')
+#
+# print("hello")
--- src/target_function.py
+++ src/target_function.py
@@ -1,5 +1,8 @@
 from create_admittance_matrix import AdmittanceMatrix
 from sympy import *
+from dynamic_equations_to_simulate import OdeSolver
+import data
+import numpy as np
 
 class ResidualVector:
 
@@ -9,13 +12,17 @@ class ResidualVector:
 
 class CovarianceMatrix:
 
-    def __init__(self, std_eps_Vm, std_eps_Va, std_eps_Im, std_eps_Ia):
-        self.std_eps_Vm = std_eps_Vm
-        self.std_eps_Va = std_eps_Va
-        self.std_eps_Im = std_eps_Im
-        self.std_eps_Ia = std_eps_Ia
+    def __init__(self, freq_data):
+        self.std_eps_Vm = freq_data.std_w_Vm
+        self.std_eps_Va = freq_data.std_w_Va
+        self.std_eps_Im = freq_data.std_w_Im
+        self.std_eps_Ia = freq_data.std_w_Ia
+        self.freqs = freq_data.freqs
+        self.time_points_len = len(self.freqs)
+
 
         self.admittance_matrix = AdmittanceMatrix().Ys
+        self.admittance_matrix_compute = lambdify(('Ef_a', 'D_Ya', 'M_Ya', 'X_Ya', 'Omega_a'), self.admittance_matrix, 'numpy')
 
         self.Y11 = self.admittance_matrix[0, 0]
         self.Y12 = self.admittance_matrix[0, 1]
@@ -34,13 +41,52 @@ class CovarianceMatrix:
         self.gamma_NrNr = None
         self.gamma_NrQr = None
 
-    def _init_gamma_NrNr(self, omega):
-        self.gamma_NrNr = (
+    def _init_gamma_NrNr(self):
+        NrNr = (
             self.std_eps_Im**2
             + self.std_eps_Vm**2 * (self.Y11r**2 + self.Y11i**2)
             + self.std_eps_Va**2 * (self.Y12r**2 + self.Y12i**2)
         )
 
+        # NrNr_compute_omega = lambdify('Omega_a', NrNr, 'numpy')
+
+        self.gamma_NrNr = zeros((self.time_points_len + 1) // 2)
+        for i in range((self.time_points_len + 1) // 2):
+            for j in range((self.time_points_len + 1) // 2):
+                if i == j:
+                    self.gamma_NrNr[i, j] = NrNr.subs('Omega_a', self.freqs[i])
+
+        # self.gamma_NrNr = self.gamma_NrNr.subs('Omega_a', omega)
+
+    def _init_gamma_NrQr(self):
+        pass
 
 
+WN = {'rnd_amp': 0.00}
+GP = {'d_2': 0.25, 'e_2': 1, 'm_2': 1, 'x_d2': 0.01, 'ic_d2': 1}
+IP = {'dt_step': 0.05, 'df_length': 100}
+OP = {'osc_amp': 2.00, 'osc_freq': 0.005}
 
+
+solver = OdeSolver(WN, GP, OP, IP)
+solver.solve()
+
+solver.simulate_time_data()
+time_data = data.TimeData(
+        Vm_time_data=solver.Vc1_abs,
+        Va_time_data=solver.Vc1_angle,
+        Im_time_data=solver.Ig_abs,
+        Ia_time_data=solver.Ig_angle,
+        dt=solver.dt
+    )
+
+time_data.apply_white_noise(snr=45.0, d_coi=0.0)
+
+print('Vm_time_data =', time_data.Vm)
+print('Im_time_data =', time_data.Im)
+print('Va_time_data =', time_data.Va)
+print('Ia_time_data =', time_data.Ia)
+
+freq_data = data.FreqData(time_data)
+
+cov_obj = CovarianceMatrix(freq_data)
