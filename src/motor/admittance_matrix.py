7e8f284280a10e1d399d69df8e94a7ebe09b0fa2
greylord1996 <emchinov@bk.ru>
2019-10-12 22:26:50 +0300

    add content

---
+++ src/motor/admittance_matrix.py
@@ -0,0 +1,65 @@
+"""Admittance matrix of a system which relates input and output data.
+An admittance matrix (denoted as Y) relates input and output data of an
+abstract dynamical system in frequency domain. These relations should be
+defined inside a class and be available as 'data' property. Due to some
+implementation details, parameters of the describing system and
+frequency variable must be accessible via 'params' and 'omega'
+properties respectively.
+Such module should be created by a user of the framework. That is,
+a wrapper around admittance matrix of a system should be defined here.
+"""
+
+import sympy as sp
+import numpy as np
+
+class AdmittanceMatrix:
+    """Admittance matrix of a generator.
+    Attributes:
+        _params (list): Contains sympy.symbols which are parameters of
+            a dynamical system (that is described by an admittance matrix).
+        _omega (sympy.symbols): a sympy variable representing frequency.
+        _admittance_matrix (sympy.Matrix): True admittance matrix
+            of a system (this class is just a wrapper around it).
+    """
+
+    def __init__(self):
+        """Define relations between input and output data."""
+        j = sp.I
+
+        R, X, H, omega = sp.symbols(
+            'R X H omega', real=True
+        )
+
+        s = j * omega
+
+        omega_e_0, p_e_0, q_e_0, sigma_0 = sp.symbols('omega_e_0 p_e_0 q_e_0 sigma_0')
+        beta = 2 * H * omega_e_0 * s + (omega_e_0 * R * (R ** 2 - sigma_0 ** 2 * X ** 2) * p_e_0) / (
+                    sigma_0 * (R ** 2 + sigma_0 ** 2 * X ** 2))
+
+        Y_dd = 1 - (2 * omega_e_0 * (R ** 2 - sigma_0 ** 2 * X ** 2)) / (sigma_0 ** 2 * R * beta)
+        Y_dq = q_e_0 + ((2 * H * (sigma_0 - 1) * s ** 2 + s) * (R ** 2 - sigma_0 ** 2 * X ** 2)) / (
+                    sigma_0 ** 2 * R * beta)
+        Y_qd = -q_e_0 + (4 * omega_e_0 * X) / (sigma_0 * beta)
+        Y_qq = 1 - ((2 * X) * (2 * H * (sigma_0 - 1) * s ** 2 + s)) / (sigma_0 * beta)
+
+        matr = sp.Matrix([[Y_dd, Y_dq], [Y_qd, Y_qq]])
+        matr = matr.subs(omega_e_0, 2 * np.pi * 50)
+        matr = matr.subs(p_e_0, 1)
+        matr = matr.subs(sigma_0, 0.04)
+        matr = matr.subs(q_e_0, 0.04950495049504951)
+        self._admittance_matrix = matr
+
+    @property
+    def params(self):
+        """sympy.symbols which are parameters of a dynamical system."""
+        return self._params
+
+    @property
+    def omega(self):
+        """sympy.symbol which represents symbol of frequency."""
+        return self._omega
+
+    @property
+    def data(self):
+        """Raw sympy.Matrix that is true admittance matrix of a system."""
+        return self._admittance_matrix
---
+++ src/motor/dynamic_equations_to_simulate.py
@@ -0,0 +1,152 @@
+import numpy as np
+from scipy.interpolate import interp1d
+from scipy.integrate import *
+import matplotlib.pyplot as plt
+
+i = 0
+j = np.complex(0, 1)
+
+
+class OdeSolver:
+
+    def __init__(self, noise, osc_param, integr_param):
+        self.noise = noise
+        self.osc_param = osc_param
+        self.IC_T1 = 0.0
+        self.IC_V1 = 1.0
+        self.dt = integr_param['dt_step']
+        self.tf = integr_param['df_length']
+        self.test_length = np.arange(0, self.tf + self.dt, self.dt)
+        self.t_vec = np.linspace(0, self.tf, self.test_length.size)
+        self.V1t = self.get_V1t()
+        self.T1t = self.get_T1t()
+        self.T1t_to_simulate = None
+        self.Ig = None
+        self.Vc1 = None
+        self.Ig_abs = None
+        self.Ig_angle = None
+        self.J = 2 * 0.5 * 0.5 / (2 * np.pi * 50) ** 2
+        self.w_0 = 1
+        self.tau = 0.02
+        self.teta = 1
+        self.wm = None
+        self.R = 0.08
+        self.z = None
+        self.X = 0.2
+        self.Pm = 1.0
+        self.we_0 = 2 * np.pi * 50
+        self.H = 0.5
+        self.sigma0 = 0.04
+        self.V0 = 1
+
+    def get_system(self, t, x):
+        w_m = x[0]
+        z = x[1]
+        A = (self.we_0 ** 2) * (self.we_0 + z / self.tau + self.T1t(t) / self.tau - w_m) * self.R * (self.V1t(t) ** 2)
+        B = 2 * self.H * (self.we_0 + z / self.tau + self.T1t(t) / self.tau) ** 2
+        D = w_m / (self.we_0 + z / self.tau + self.T1t(t) / self.tau)
+        C = self.R ** 2 + (1 - D) ** 2 * self.X ** 2
+
+        dwmdt = A / (B * C) - self.Pm * self.we_0 / (self.H * 2)
+        dzdt = 0 - z / self.tau - self.T1t(t) / self.tau
+
+        return [dwmdt, dzdt]
+
+    def solve(self):
+        k = self.tau * 2 * np.pi * 50 - self.tau * 2 * np.pi * 50 * 0.999 - 0.001
+        y0 = [2 * np.pi * 50 * 0.96, k]
+        # start_time = time.time()
+        # print("--- %s seconds ---" % (time.time() - start_time))
+        self.sol = solve_ivp(fun=self.get_system, t_span=[0, self.tf], y0=y0, t_eval=self.t_vec)
+        # print("--- %s seconds ---" % (time.time() - start_time))
+        self.w_m = self.sol.y[0, :]
+        self.z = self.sol.y[1, :]
+        return {'wm': self.w_m, 'z': self.z}
+
+    def get_V1t(self):
+        vn_vec = np.random.normal(0, 1, self.test_length.size) * self.noise['rnd_amp'] + self.IC_V1
+        V1t = interp1d(self.t_vec, vn_vec, kind='cubic', fill_value="extrapolate")
+        return V1t
+
+    def show_V1t_in_test_mode(self):
+        plt.plot(self.t_vec, self.V1t(self.t_vec))
+        plt.legend(['V1t'])
+        plt.show()
+
+    def get_T1t(self):
+        tn_vec = np.random.normal(0, 1, self.test_length.size) * self.noise['rnd_amp'] + self.IC_T1
+        T1t = interp1d(self.t_vec, tn_vec, kind='cubic', fill_value="extrapolate")
+        return T1t
+
+    def get_t_vec(self):
+        return self.t_vec
+
+    def show_T1t_in_test_mode(self):
+        plt.plot(self.t_vec, self.T1t(self.t_vec))
+        plt.legend(['T1t'])
+        plt.show()
+
+    def show_results_in_test_mode(self):
+        # plt.figure()
+        plt.plot(self.t_vec, self.w_m)
+        plt.legend(['w_m(t)'])
+        plt.show()
+        plt.plot(self.t_vec, self.z)
+        plt.legend(['z(t)'])
+        plt.show()
+        plt.plot(self.t_vec, self.T1t(self.t_vec))
+        plt.legend(['teta(t)'])
+        plt.show()
+        plt.plot(self.t_vec, self.pe)
+        plt.legend(['pe(t)'])
+        plt.show()
+        plt.plot(self.t_vec, self.we)
+        plt.legend(['we(t)'])
+        plt.show()
+        plt.plot(self.t_vec, self.sigma)
+        plt.legend(['sigma(t)'])
+        plt.show()
+        plt.plot(self.t_vec, self.id)
+        plt.legend(['id(t)'])
+        plt.show()
+        plt.plot(self.t_vec, self.iq)
+        plt.legend(['iq(t)'])
+        plt.show()
+        # plt.savefig('data.pdf', format='pdf')
+
+        # plt.plot(self.t_vec, self.Ig)
+        # plt.legend(['Ig(t)'])
+        # plt.show()
+        # plt.plot(self.t_vec, self.Vc1)
+        # plt.legend(['Vc1(t)'])
+        # plt.show()
+
+    def simulate(self):
+        self.solve()
+        self.we = self.we_0 + self.z / self.tau + self.T1t(self.t_vec) / self.tau
+        self.sigma = 1 - self.w_m / self.we
+        self.pe = (self.sigma * self.R * self.V1t(self.t_vec) ** 2) / (self.R ** 2 + self.sigma ** 2 * self.X ** 2)
+        self.I_0 = 1 / ((self.R / self.sigma0) + j * self.X)
+        self.qe0 = 0 - self.I_0.imag
+        self.I = self.V1t(self.t_vec) / (self.R / self.sigma + j * self.X)
+        self.id = self.I.real
+        self.iq = self.I.imag
+
+
+ode_solver_object = OdeSolver(
+    noise={
+        'rnd_amp': 0.002,
+        'snr': 45.0
+    },
+    osc_param={
+        'osc_amp': 0.000,
+        'osc_freq': 0.0
+    },
+    integr_param={
+        'df_length': 200.0,
+        'dt_step': 0.001
+    }
+)
+
+ode_solver_object.simulate()
+ode_solver_object.show_results_in_test_mode()
